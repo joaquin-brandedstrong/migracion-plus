5f1938c69b10c0901a40193437735025c6e8daa7
joaquin-brandedstrong <joaquin@brandedstrong.com>
2026-05-05 18:54:38 -0300

    Fix ESLint config: rename to .cjs extension for CommonJS compatibility

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['next/core-web-vitals', '../../packages/config/eslint.config.js'],
+  extends: ['next/core-web-vitals', '../../packages/config/eslint.config.cjs'],
   parserOptions: {
     project: './tsconfig.json',
     tsconfigRootDir: __dirname,
